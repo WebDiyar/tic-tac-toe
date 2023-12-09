9496b007b8884ee01bc8abc750eeb2d2b2ce6a06
WebDiyar <diyar.atashka@mail.ru>
2023-12-09 21:01:05 +0600

    add X and Y images for button

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,19 +1,25 @@
+import Cross from '../assets/cross.png';
+import Circle from '../assets/circle.png';
+
 const GameBoard = ({ onSelectSquare, board }) => {
-    return (
-        <ol id='game-board'>
-            {board.map((row, rowIndex) => (
-                <li key={rowIndex}>
-                    <ol>
-                        {row.map((playerSymbol, colIndex) => (
-                            <li key={colIndex}>
-                                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null} >{playerSymbol}</button>
-                            </li>
-                        ))}
-                    </ol>
-                </li>
+  return (
+    <ol id='game-board'>
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null} >
+                  {playerSymbol === 'X' ? <img src={Cross} alt="Cross" /> : null}
+                  {playerSymbol === 'O' ? <img src={Circle} alt="Circle" /> : null}
+                </button>
+              </li>
             ))}
-        </ol>
-    )
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
 
-export default GameBoard
+export default GameBoard;
